940f9b7cae07ebe1bde23f76f13b7323d9a860d7
AnJiIn <aji0719@gmail.com>
2021-11-28 03:20:08 +0900

    challenge6

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos(){
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event){
 const li = event.target.parentElement;
 li.remove();
@@ -14,7 +22,6 @@ function paintToDo(newTodo) {
   const button = document.createElement("button");
   button.innerText="❌";
   button.addEventListener("click", deleteToDo);
-
   li.appendChild(span);
   li.appendChild(button);
   toDoList.appendChild(li);
@@ -24,6 +31,17 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
-toDoForm.addEventListener("submit", handleToDoSubmit)
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(saveToDos !== null){
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach(paintToDo);
+}
