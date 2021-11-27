180e540bf9538b8acd5831a1e00348cc7553b349
AnJiIn <aji0719@gmail.com>
2021-11-27 14:31:29 +0900

    challenge3

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,5 @@
 const clock = document.querySelector("#clock");
-
+const timeGreeting = document.querySelector("#time-greeting")
 
 function getClock() {
   const date = new Date();
@@ -7,6 +7,14 @@ function getClock() {
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   clock.innerText = `${hours} : ${minutes} : ${seconds}`;
+
+  if(hours>=5 && hours<12){
+    timeGreeting.innerText= "Good Morning!";
+  }else if(hours>=12 && hours<18){
+    timeGreeting.innerText= "Good Afternoon!";
+  }else{
+    timeGreeting.innerText= "Good Evening!";
+  }
 }
 getClock();
 setInterval(getClock, 1000);
